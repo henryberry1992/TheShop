53d1a10e768a5eb0f359251155668a0b287c3b78
henryberry1992 <henryberry1992@gmail.com>
2016-01-24 13:44:54 -0500

    changed store css grid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
+  
+  def create
+  	@user = User.new(params[:user])
+  	respond_to do |format|
+  		if @user.save
+  			format.html {redirect_to users_url,
+  				notice: "Welcome, #{@user.name}."}
+  			format.json {render json: @user,
+  				status: :created, location: @user }
+  		else
+  			format.html { render action: "new" }
+  			format.json { render json: @user.erros,
+  				status: :unprocessable_entity }
+  		end
+  	end
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	respond_to do |format|
+  		if @user.update_attributes(params[:user])
+  			format.html { redirect_to users_url, 
+  				notice: "Change updated."}
+  			format.json { head :no_content}
+  		else
+  			format.html { render action: "edit"}
+  			format.json { render json: @user.errors,
+  				status: :unprocessable_entity }
+  		end
+  	end
+  end
+  
   def new
   end
 
